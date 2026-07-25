44743182fec303e0a8679190df6db8bbbade08df
AlyHacks <alyasu929@gmail.com>
2026-07-24 23:19:11 -0400

    removed and cleaned up for loop

--- software/Fusion/Pi/direction_wout_time.py
+++ software/Fusion/Pi/direction_wout_time.py
@@ -16,6 +16,7 @@ setup1 = time.time()
 ledl = LED(5)
 ledr = LED(4)
 
+#creation of buffers and the fused dictionary to store the matched rgb and sensor frames
 camera_buffer = []
 tof_buffer = []
 fused = {"timestamp": 0, "distance": 0, "object": 0}
@@ -33,6 +34,8 @@ sensor.timing_budget = 50
 picam2 = Picamera2() 
 model = YOLO('yolov8n.pt')
 
+
+#configure the picam2 settings to be the correct format of 640x480 and RGB888 for the yolo model to work properly
 picam2.configure(
     picam2.create_preview_configuration(
         main={"size": (640, 480), "format": "RGB888"}
@@ -61,6 +64,8 @@ def buzzing(cx, distance, ledr, ledl):
         ledl.off()
 
 def max_area(x1, x2, y1, y2, areas, last_ten_box):
+
+    
     last_ten_box.append((x1,y1,x2,y2))
                     
     area = (x2-x1)*(y2-y1)
@@ -80,23 +85,16 @@ def max_area(x1, x2, y1, y2, areas, last_ten_box):
     cx = x_1+(x_2-x_1)/2  
     return cx, x_1, y_1, x_2, y_2
 
-def position(timestamp_c, distance, results, last_three_c, correct_index, ledr, ledl, areas, last_ten_box):
-    pos1 = time.time()
-    for timestamp_c, distance, results in last_three_c[correct_index-1]:#iterates througuh the correct camera frame
-            for box in results.boxes:
-                class_id = int(box.cls[0])
-                class_name = model.names[class_id]
-                fused["object"] = class_name  #gets object
-                x1, y1, x2, y2= box.xyxy[0]
-                
-                x1 = x1.item()
-                x2 = x2.item()
-                y1 = y1.item()
-                y2 = y2.item()
-                
-                cx, x_1, x_2, y_1, y_2 = max_area(x1, x2, y1, y2, areas, last_ten_box)                       
-                buzzing(cx, distance, ledr, ledl)
-
+def frame_calc(timestamp_c,last_s, compare, last_time_s, last_three_c, fused):
+        difference = abs(timestamp_c-last_time_s) #finds the closest camrea frame timestamp to the closest sensor reading
+        compare.append(difference) #stores it in a compare list to compare the three differences
+        
+        correct_index = compare.index(min(compare)) #finds the index of the minimum 0->-3, 1->-2, 2->-1
+        
+        fused["timestamp"] = last_three_c[correct_index-1][0] #gets the correct timestamp of camera frame,        
+        fused["distance"] = last_s[1] #takes the distance from the last_s tuple
+        distance = fused["distance"]
+        return distance, correct_index, fused
 
 
 picam2.start()
@@ -128,25 +126,29 @@ def main(ledr, ledl, sensor, picam2, model, camera_buffer, tof_buffer, compare, 
 
         tof_buffer.append((timestamp_s, distance))
         sensor.clear_interrupt()
-        
+
         last_three_c = list(camera_buffer)[-3:] #takes the latest 3 camera frames
         last_s = tof_buffer[-1] #takes the last sensor frame
         last_time_s = last_s[0]
-        difference = abs(timestamp_c-last_time_s) #finds the closest camrea frame timestamp to the closest sensor reading
-        compare.append(difference) #stores it in a compare list to compare the three differences
-        
-        correct_index = compare.index(min(compare)) #finds the index of the minimum 0->-3, 1->-2, 2->-1
-        
-        fused["timestamp"] = last_three_c[correct_index-1][0] #gets the correct timestamp of camera frame,        
-        fused["distance"] = last_s[1] #takes the distance from the last_s tuple
-        distance = fused["distance"]
+
+        distance, correct_index, fused = frame_calc(timestamp_c,last_s, compare, last_time_s, last_three_c, fused)
+
+        for timestamp_c, distance, results in last_three_c[correct_index-1]:#iterates througuh the correct camera frame
+                for box in results.boxes:
+                    class_id = int(box.cls[0])
+                    class_name = model.names[class_id]
+                    fused["object"] = class_name  #gets object
+                    x1, y1, x2, y2= box.xyxy[0]
+                    
+                    x1 = x1.item()
+                    x2 = x2.item()
+                    y1 = y1.item()
+                    y2 = y2.item()
+                    
+                    cx, x_1, x_2, y_1, y_2 = max_area(x1, x2, y1, y2, areas, last_ten_box)                       
+                    buzzing(cx, distance, ledr, ledl)
 
         
-        loop1 = time.time()
-        pos1 = time.time()
-        position(ledr, ledl, sensor, picam2, model, camera_buffer, tof_buffer, compare, last_ten_box, fused)
-            
-    
         
         if cv2.waitKey(1) == ord('q'):
             print("error")
