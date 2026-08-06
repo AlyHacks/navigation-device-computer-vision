ef107e42a54387dd7b514983658c3499cd99a43a
AlyHacks <alyasu929@gmail.com>
2026-08-06 05:44:46 -0400

    new mermaid diagram

---
+++ software/Flowchart/v12.js
@@ -0,0 +1,75 @@
+---
+config:
+  layout: dagre
+---
+flowchart TB
+ subgraph Legend["Legend"]
+    direction TB
+        a1["🔴 Capture step"]
+        a2["🟡 Data/result node"]
+        a3["🟢 Processing step"]
+        a4["🔵 Output/Action"]
+  end
+    A["RGB sensor reading"] --> n6["Timestamp"] & n21["Camera Buffer"]
+    n6 --> B("Timestamp comparison")
+    n1["LiDAR sensor reading"] --> n7["Timestamp"] & n10["Time-synchronized LiDAR frame"]
+    n7 --> B
+    C["Dictionary of timestamp-matched camera and LiDAR frames"] --> n9["Time-synchronized camera frame"]
+    B --> C
+    n9 --> n28["Distance detected?"]
+    n21 --> C
+    n10 --> n28
+    n40["Position index"] --> n41["If 0&lt;index&lt;320, left buzzer buzzes with consistent frequency"] & n42["If 320&lt;index&lt;640, right buzzer buzzes with consistent frequency"]
+    n43["Distance &lt; 1000"] -- NO --> n44["No output"]
+    n28 -- NO --> n47["Object detected?"]
+    n47 -- YES --> n50["Object localization"]
+    n47 -- NO --> n51["No ouput"]
+    n50 --> n40
+    n28 -- YES --> n43
+    n43 -- YES --> n46["Object detected?"]
+    n46 -- YES --> n52["Object localization"]
+    n46 -- NO --> n38["Both buzzers output buzzing frequency relative to distance"]
+    n52 --> n53["Position index"]
+    n53 --> n54["Untitled Node"] & n55["If 320&lt;index&lt;640, right buzzer buzzes with frequency relative to distance"]
+
+    A@{ shape: lean-r}
+    n1@{ shape: lean-r}
+    C@{ shape: event}
+    n28@{ shape: rounded}
+    n43@{ shape: diam}
+    n47@{ shape: rounded}
+    n50@{ shape: rounded}
+    n46@{ shape: rounded}
+    n52@{ shape: rounded}
+    n55@{ shape: rect}
+     a1:::capture
+     a2:::data
+     a3:::process
+     a4:::output
+    classDef capture fill:#FFCDD2,stroke:#F44336
+    classDef data fill:#FFF9C4,stroke:#FBC02D
+    classDef process fill:#C8E6C9,stroke:#388E3C
+    classDef output fill:#BBDEFB,stroke:#1976D2
+    style A fill:#FFCDD2
+    style n6 fill:#FFF9C4
+    style n21 fill:#FFF9C4
+    style B fill:#C8E6C9
+    style n1 fill:#FFCDD2
+    style n7 fill:#FFF9C4
+    style n10 fill:#FFF9C4
+    style C fill:#C8E6C9
+    style n9 fill:#FFF9C4
+    style n28 fill:#C8E6C9
+    style n40 fill:#FFF9C4
+    style n41 fill:#BBDEFB
+    style n42 fill:#BBDEFB
+    style n43 fill:#C8E6C9
+    style n44 fill:#BBDEFB
+    style n47 fill:#C8E6C9
+    style n50 fill:#C8E6C9
+    style n51 fill:#BBDEFB
+    style n46 fill:#C8E6C9
+    style n52 fill:#C8E6C9
+    style n38 fill:#BBDEFB
+    style n53 fill:#FFF9C4
+    style n55 fill:#BBDEFB
