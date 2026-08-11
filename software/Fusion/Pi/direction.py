ce6d1b146921cce4ffedc59b5c7e7f803336d38a
AlyHacks <alyasu929@gmail.com>
2026-08-10 23:58:46 -0400

    added forgotten for loop in result in results

--- software/Fusion/Pi/direction.py
+++ software/Fusion/Pi/direction.py
@@ -84,8 +84,9 @@ def max_area(x1, x2, y1, y2, areas, last_ten_box):
 
 def position(timestamp_c, distance, results, last_three_c, correct_index, ledr, ledl, areas, last_ten_box, fused):
     pos1 = time.time()
-    for timestamp_c, distance, results in last_three_c[correct_index-1]:#iterates througuh the correct camera frame
-            for box in results.boxes:
+    for timestamp_c, frame, results in last_three_c:#[correct_index]:#iterates througuh the correct camera frame
+        for result in results:
+            for box in result.boxes:
                 class_id = int(box.cls[0])
                 class_name = model.names[class_id]
                 fused["object"] = class_name  #gets object
